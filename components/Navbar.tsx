ba238a128601a2b47b1485ad75deb96c745a3b1d
sachin-malani <sachinmalani32@gmail.com>
2024-08-10 10:26:46 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <nav className="justify-between fixed z-40 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link href='/' className="flex items-center gap-1">
-        <Image src="/icons/logo.svg" alt="logo" width={32} height={32} className="max-sm:size-10"/>
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Video Convo</p>
+    <nav className="flex-between fixed z-40 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Video Convo
+        </p>
       </Link>
-
+      <div className="flex-between gap-5">
+        <MobileNav />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

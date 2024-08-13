731617a9cda98b66d0f42f2eb948506c68823107
sachin-malani <sachinmalani32@gmail.com>
2024-08-13 19:18:54 +0530

    Toast for alerts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ClerkProvider appearance={{
-        layout: {
-          logoImageUrl: '/icons/yoom-logo.svg',
-          socialButtonsVariant: 'iconButton'
-        },
-        variables: {
-        colorText: '#fff',
-        colorPrimary: '#0E78F9',
-        colorBackground: '#1C1F2E',
-        colorInputBackground: '#252A41',
-        colorInputText: '#fff'
-      }}}>
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: "/icons/yoom-logo.svg",
+            socialButtonsVariant: "iconButton",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </ClerkProvider>
     </html>
   );
